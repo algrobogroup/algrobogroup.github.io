8856e7fc9113aee2e97abc9cec8d84d14a8f2ab5
syc7446 <syc7446@gmail.com>
2026-03-07 14:30:39 +0000

    Deploying to gh-pages from @ algrobogroup/algrobogroup.github.io@7596a517e7aba943e7f443286cbbaa0858ec038d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,7 +10,7 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-people",
-          title: "People",
+          title: "people",
           description: "",
           section: "Navigation",
           handler: () => {
